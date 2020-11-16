0c936a82a0d154aa82dcb5e62c3a6e8edb6cb4ca
Madi Messmore <55211235+madimessmore@users.noreply.github.com>
2020-11-16 11:09:56 -0500

    Update display_test.ino

--- display_test.ino
+++ display_test.ino
@@ -250,7 +250,7 @@ void loop() {
   tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );       //  Temp Kelvin
   float tempC = tempK - 273.15;            // Convert Kelvin to Celcius
   float tempF = (tempC * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
-  Serial.print("Temperature is %d\n", tempF);
+  Serial.print("Temperature is %lf \n", tempF);
 	
 	writeNum(tempF);
 	delay(1);
