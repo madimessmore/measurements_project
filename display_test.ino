63ff76766423feb25840c1b95cadb5c54dfec44d
Madi Messmore <55211235+madimessmore@users.noreply.github.com>
2020-11-16 13:50:53 -0500

    Update display_test.ino

--- display_test.ino
+++ display_test.ino
@@ -246,7 +246,7 @@ void writeNum(int num)
 int getTemp()
 {
   int tempReading = analogRead(tempPin);
-  tempReading -= 940;
+  tempReading -= 981;
   double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
   tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );       //  Temp Kelvin
   float tempC = tempK - 273.15;            // Convert Kelvin to Celcius
